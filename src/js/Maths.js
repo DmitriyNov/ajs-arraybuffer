9b5ff598eb7bedd0824204e1ab82a576abf2b461
DmitriyNov <NovokrDm@yandex.ru>
2023-03-12 10:41:47 +0300

    Add set attack function

--- src/js/Maths.js
+++ src/js/Maths.js
@@ -6,6 +6,18 @@ export default class Maths {
     this.stoned = false;
   }
 
+  set attack(value) {
+    // Я согласен, что логика здесь может быть не совсем такая, как требуется по заданию,
+    // но я не знаю, как сделать, чтобы при задании атаки вручную с учётом дистанции и дурмана и
+    // при этом не сломать остальные сеттеры. Пусть будет так, что здесь задаём конкретное значение
+    this._attack = value;
+    this.baseAttack = value;
+  }
+
+  get attack() {
+    return this._attack;
+  }
+
   set distance(value) {
     this.attack = Math.floor(this.baseAttack * (1.1 - value * 0.1));
     this._distance = value;
@@ -19,11 +31,11 @@ export default class Maths {
     // Тут логика такая, что если дебафаем персонажа и на нём сейчас нет дебафа, то он применяется,
     // а если он уже есть, то никак не влияет
     if (value && !this.stoned) {
-      this.attack = Math.floor(this.attack - Math.log2(this.distance) * 5);
+      this.attack = Math.floor(this.baseAttack - Math.log2(this.distance) * 5);
     // Тут соответственно наоборот, если дебаф снимем и он есть, то атака увеличивается
     } else if (!value && this.stoned) {
       // Эта строка у меня не покрыта, но по заданию такого вроде и не требовалось реализовывать
-      this.attack = Math.floor(this.attack + Math.log2(this.distance) * 5);
+      this.attack = Math.floor(this.baseAttack + Math.log2(this.distance) * 5);
     }
     this._stoned = value;
   }
